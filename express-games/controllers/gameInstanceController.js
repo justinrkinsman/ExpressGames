3edcb79c12f804e5ce15a1935577acd7a1554705
Justin <justinrkinsman@gmail.com>
2022-11-14 09:28:35 -0400

    Add content to gameInstance controller

---
+++ express-games/controllers/gameInstanceController.js
@@ -0,0 +1,41 @@
+const GameInstance = require("../models/gameinstance")
+
+// Display list of all GameInstances.
+exports.gameinstance_list = (req, res) => {
+    res.send("NOT IMPLEMENTED: GameInstance list")
+}
+
+// Display detail page for a specific GameInstance.
+exports.gameInstance_detail = (req, res) => {
+    res.send(`NOT IMPLEMENTED: GameInstance detail: ${req.params.id}`)
+}
+
+// Display GameInstance create form on GET.
+exports.gameInstance_create_get = (req, res) => {
+    res.send("NOT IMPLEMENTED: GameInstance create GET")
+}
+
+// Handle GameInstance create on POST.
+exports.gameInstance_create_post = (req, res) => {
+    res.send("NOT IMPLEMENTED: GameInstance create POST")
+}
+
+// Display GameInstance delete form on GET.
+exports.gameInstance_delete_get = (req, res) => {
+    res.send("NOT IMPLEMENTED: GameInstance delete GET")
+}
+
+// Handle GameInstance delete on POST.
+exports.gameInstance_delete_post = (req, res) => {
+    res.send("NOT IMPLEMENTED: GameInstance delete POST")
+}
+
+// Display GameInstance update form on GET.
+exports.gameInstance_update_get = (req, res) => {
+    res.send("NOT IMPLEMENTED: GameInstance update GET")
+}
+
+// Handle GameInstance update on POST.
+exports.gameInstance_update_post = (req, res) => {
+    res.send("NOT IMPLEMENTED: GameInstance update POST")
+}
