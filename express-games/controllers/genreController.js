96c95aa85936abdb1787d0637d1954296a0bb715
Justin <justinrkinsman@gmail.com>
2022-11-15 11:45:20 -0400

    Add release_date_formatted virtul to game model

--- express-games/controllers/genreController.js
+++ express-games/controllers/genreController.js
@@ -1,4 +1,6 @@
 const Genre = require("../models/genre")
+const Game = require("../models/game")
+const async = require('async')
 
 // Display list of all Genres.
 exports.genre_list = function (req, res, next) {
@@ -18,8 +20,35 @@ exports.genre_list = function (req, res, next) {
 }
 
 // Display detail page for a specific Genre.
-exports.genre_detail = (req, res) => {
-    res.send(`NOT IMPLEMENTED: Genre detail: ${req.params.id}`)
+exports.genre_detail = function (req, res, next) {
+    async.parallel(
+        {
+            genre: function (callback) {
+                Genre.findById(req.params.id).exec(callback)
+            },
+
+            genre_games: function(callback) {
+                Game.find({ genre: req.params.id }).exec(callback)
+            },
+        },
+        function (err, results) {
+            if (err) {
+                return next(err)
+            }
+            if (results.genre == null) {
+                // No results.
+                const err = new Error("Genre not found")
+                err.status = 404
+                return next(err)
+            }
+            // Successful, so render
+            res.render("genre_detail", {
+                title: "Genre Detail",
+                genre: results.genre,
+                genre_games: results.genre_games
+            })
+        }
+    )  
 }
 
 // Display Genre create form on GET.
--- express-games/models/game.js
+++ express-games/models/game.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose")
+const { DateTime } = require('luxon')
 
 const Schema = mongoose.Schema
 
@@ -15,6 +16,10 @@ const GameSchema = new Schema({
 // Virtual for console's URL
 GameSchema.virtual("url").get(function () {
     return "/catalog/game/" + this.id
+})
+
+GameSchema.virtual("release_date_formatted").get(function () {
+    return DateTime.fromJSDate(this.release_date).toLocaleString(DateTime.DATE_MED)
 })
 
 module.exports = mongoose.model("Game", GameSchema)
