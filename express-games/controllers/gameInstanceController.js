d197f77f6b13b7afe3f2d4a80e65d72265b2c1a0
Justin <justinrkinsman@gmail.com>
2022-11-17 10:27:37 -0400

    Create add game instance form

--- express-games/controllers/gameInstanceController.js
+++ express-games/controllers/gameInstanceController.js
@@ -1,4 +1,6 @@
 const GameInstance = require("../models/gameinstance")
+const { body, validationResult } = require("express-validator")
+const Game = require("../models/game")
 
 // Display list of all GameInstances.
 exports.gameInstance_list = function (req, res, next) {
@@ -39,14 +41,64 @@ exports.gameInstance_detail = (req, res, next) => {
     }
 
 // Display GameInstance create form on GET.
-exports.gameInstance_create_get = (req, res) => {
-    res.send("NOT IMPLEMENTED: GameInstance create GET")
+exports.gameInstance_create_get = (req, res, next) => {
+    Game.find({}, "title").exec((err, games) => {
+        if (err) {
+            return next(err)
+        }
+        // Successful, so render.
+        res.render("gameinstance_form", {
+            title: "Create GameInstance",
+            game_list: games,
+        })
+    })
 }
 
 // Handle GameInstance create on POST.
-exports.gameInstance_create_post = (req, res) => {
-    res.send("NOT IMPLEMENTED: GameInstance create POST")
-}
+exports.gameInstance_create_post = [
+    // Validate and sanitize fields.
+    body("game", "Game must be specified").trim().isLength({ min: 1}).escape(),
+    body("status").escape(),
+
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+        // Extract the validation errors from a request.
+        const errors = validationResult(req)
+
+        // Create a GameInstance object with escaped and trimmed data.
+        const gameinstance = new GameInstance({
+            game: req.body.game,
+            status: req.body.status,
+        })
+
+        if (!errors.isEmpty()) {
+            // There are errors. Render form again with sanitized values and error messages.
+            Game.find({}, "title").exec(function (err, games) {
+                if (err) {
+                    return next(err)
+                }
+                // Successful, so render.
+                res.render("gameinstance_form", {
+                    title: "Create GameInstance",
+                    game_list: games,
+                    selected_game: gameinstance.game._id,
+                    errors: errors.array(),
+                    gameinstance,
+                })
+            })
+            return
+        }
+
+        // Data from form is valid.
+        gameinstance.save((err) => {
+            if (err) {
+                return next(err)
+            }
+            // Successful: redirect to new record.
+            res.redirect(gameinstance.url)
+        })
+    },
+]
 
 // Display GameInstance delete form on GET.
 exports.gameInstance_delete_get = (req, res) => {
