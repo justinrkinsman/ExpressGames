40c820d938ab18834d876ac6ed299d500c3dbaa9
Justin <justinrkinsman@gmail.com>
2022-11-17 09:19:25 -0400

    Fix multiple genre bug

--- express-games/models/game.js
+++ express-games/models/game.js
@@ -6,7 +6,7 @@ const Schema = mongoose.Schema
 const GameSchema = new Schema({
     title: { type: String, required: true },
     console: { type: Schema.Types.ObjectId, ref: "Console", required: true },
-    genre: { type: Schema.Types.ObjectId, ref: "Genre", required: true },
+    genre: [{ type: Schema.Types.ObjectId, ref: "Genre", required: true }],
     developer: { type: String },
     publisher: { type: String },
     release_date: { type: Date },
